efa345a35b438c7edb4eb00f2acd91539e4c61ef
vagrant <vagrant@localhost.localdomain>
2020-07-19 07:01:16 +0000

    マイページ　編集

--- app/controllers/post_historys_controller.rb
+++ app/controllers/post_historys_controller.rb
@@ -1,16 +1,26 @@
 class PostHistorysController < ApplicationController
+  before_action :authenticate_user!
   def index
+    @post_histories = current_user.post_histories
   end
 
   def show
+    @post_history = PostHistory.find(params[:id])
   end
 
   def edit
+    @post_history = PostHistory.find(params[:id])
   end
 
   def destroy
+    post_history = PostHistory.find(params[:id])
+    post_history.destroy
+    redirect_to  post_histories_path
   end
 
   def update
+    post_history = PostHistory.find(params[:id])
+    post_history.update(post_history_params)
+    redirect_to post_histories_path
   end
 end
