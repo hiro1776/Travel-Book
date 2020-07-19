7b8e818e37d4f7dedd31fb74c851272f5a004593
vagrant <vagrant@localhost.localdomain>
2020-07-19 06:10:28 +0000

    投稿履歴テーブル作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,4 +5,16 @@ class UsersController < ApplicationController
 	end
 	def edit
 	end
+	def update
+		@user = current_user
+		if @user.update(user_params)
+			flash[:success] = "登録情報を変更しました。"
+			redirect to user_path
+		else
+			render 'edit'
+	end
+	private
+	def user_params
+		params.require(:user).permit(:name,:email)
+	end
 end
